57e83abc4a2b07d40c9dc51112369e7b5628f69f
Peter Cho <pcho231@gmail.com>
2019-07-09 00:48:17 -0400

    Added cheat notes.

--- src/03-e2e/ProprietaryMethod.js
+++ src/03-e2e/ProprietaryMethod.js
@@ -6,3 +6,4 @@ const Up = () => 'Up';
 const ProprietaryMethod = (who = 'You') => `${Never()} ${Going} ${GiveWho(who)} ${Up()}`;
 
 export default ProprietaryMethod;
+// export { ProprietaryMethod, Going };
--- src/03-e2e/ProprietaryMethod.test.js
+++ src/03-e2e/ProprietaryMethod.test.js
@@ -1,4 +1,5 @@
 import ProprietaryMethod from './ProprietaryMethod';
+// import { Going, ProprietaryMethod } from './ProprietaryMethod';
 
 it('runs ProprietaryMethod and returns a number', () => {
   expect(ProprietaryMethod()).toEqual('Never Gonna Give You Up');
