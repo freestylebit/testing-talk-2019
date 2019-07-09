05e11bf7c21bc81d300416cae0903037a65c9e43
Peter Cho <pcho231@gmail.com>
2019-07-09 00:42:55 -0400

    Added 03-e2e

---
+++ src/03-e2e/ProprietaryMethod.js
@@ -0,0 +1,8 @@
+const Never = () => 'Never';
+const Going = 'Gonna';
+const GiveWho = who => `Give ${who}`;
+const Up = () => 'Up';
+
+const ProprietaryMethod = (who = 'You') => `${Never()} ${Going} ${GiveWho(who)} ${Up()}`;
+
+export default ProprietaryMethod;
