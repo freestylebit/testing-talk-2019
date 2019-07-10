1b1d1cb4e5379467fab301a1be49fa64ce71f94f
Peter Cho <pcho231@gmail.com>
2019-07-10 02:14:03 -0400

    Rearranged test to be more brittle

--- src/05-implementation/Users.test.js
+++ src/05-implementation/Users.test.js
@@ -11,16 +11,20 @@ import VeryComplicatedCode from './VeryComplicatedCode';
 //    Users().then(data => expect(data).toEqual('Rick Astley'));
 // });
 
-// jest.mock('./VeryComplicatedCode');
+jest.mock('./VeryComplicatedCode');
 
 it('should increment dynamically', () => {
-  // VeryComplicatedCode.mockImplementation(() => 9);
   const generator = Users();
+
+  VeryComplicatedCode.mockImplementation(() => 7);
 
   expect(generator.next().value).toEqual(0);
   expect(generator.next().value).toEqual(1);
   expect(generator.next().value).toEqual(2);
   expect(generator.next().value).toEqual(3);
   expect(generator.next().value).toEqual(11);
-  expect(generator.next().value).toEqual(18);
+
+  VeryComplicatedCode.mockImplementation(() => 9);
+
+  expect(generator.next().value).toEqual(20);
 });
