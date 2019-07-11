54bd9cbf339e02372a9ef7735b079bb5f17f6ed7
Peter Cho <pcho231@gmail.com>
2019-07-11 01:20:41 -0400

    More lyrics

--- src/07-specificity/Specificity.test.js
+++ src/07-specificity/Specificity.test.js
@@ -1,7 +1,35 @@
 import Specificity from './Specificity';
 
 it('tests that the JSON payload matches', () => {
-  const payload = ['together', 'forever', 'and', 'never', 'to', 'part'];
+  const payload = [
+    'together',
+    'forever',
+    'and',
+    'never',
+    'to',
+    'part',
+    'together',
+    'forever',
+    'we',
+    'two',
+    'and',
+    "don't",
+    'you',
+    'know',
+    'I',
+    'would',
+    'move',
+    'heaven',
+    'and',
+    'earth',
+    'to',
+    'be',
+    'together',
+    'forever',
+    'with',
+    'you',
+  ];
 
-  expect(Specificity).toEqual(payload);
+  expect(Specificity()).toEqual(payload);
+  // Try `.toContain` here
 });
